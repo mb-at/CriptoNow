548e769169431a93e3b0c5d5f8ecc3e9e9941762
Mario <mario.agra@plcmadrid.es>
2021-10-09 18:27:35 +0200

    Prueba de que el repositorio de gitHub está sicronizado correctamente con mi proyecto.

--- app/src/main/java/com/example/criptonow/MainActivity.kt
+++ app/src/main/java/com/example/criptonow/MainActivity.kt
@@ -5,7 +5,14 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //Hola GitHub, esto es un comentario de prueba.
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    private fun saludar(){
+
+        println("Hola GitHub");
     }
 }
